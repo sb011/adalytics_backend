39b98898d3d8eb30f266aace2091c46e19740b90
Darshit1019 <201901019@daiict.ac.in>
2024-07-06 18:56:19 +0530

    feature/roleBasedAccess-commit-1

--- src/main/java/com/adalytics/adalytics_backend/constants/CommonConstants.java
+++ src/main/java/com/adalytics/adalytics_backend/constants/CommonConstants.java
@@ -4,4 +4,6 @@ public class CommonConstants {
     public static final String DEFAULT_USERNAME = "guest";
     public static final int JWT_TOKEN_EXPIRY = 1000 * 60 * 60 * 24 * 7;
     public static final int EMAIL_TOKEN_EXPIRY = 1000 * 60 * 15;
+    public static final String ROLE = "ROLE";
+    public static final String UNDERSCORE = "_";
 }
