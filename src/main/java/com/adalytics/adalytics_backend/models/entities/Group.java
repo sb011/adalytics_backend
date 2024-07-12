25052ee21314aedf417fcbc1fef74684a35e3222
sb011 <smitbhoraniya11@gmail.com>
2024-07-12 19:56:03 +0530

    added the created and updatedat

--- src/main/java/com/adalytics/adalytics_backend/models/entities/Group.java
+++ src/main/java/com/adalytics/adalytics_backend/models/entities/Group.java
@@ -2,8 +2,12 @@ package com.adalytics.adalytics_backend.models.entities;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -13,4 +17,8 @@ public class Group {
     private String id;
     private String name;
     private String organizationId;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/adalytics/adalytics_backend/models/entities/Metric.java
+++ src/main/java/com/adalytics/adalytics_backend/models/entities/Metric.java
@@ -2,8 +2,12 @@ package com.adalytics.adalytics_backend.models.entities;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -15,4 +19,8 @@ public class Metric {
     private String metricType;
     private String verticalAxisProperty;
     private String groupId;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
 }
