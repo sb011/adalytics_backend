ddd61967bc59b46259210989cc65583e4f544d1b
sb011 <smitbhoraniya11@gmail.com>
2024-07-21 17:28:22 +0530

    get token from query params in web socket

--- src/main/java/com/adalytics/adalytics_backend/utils/NotificationHandler.java
+++ src/main/java/com/adalytics/adalytics_backend/utils/NotificationHandler.java
@@ -1,7 +1,9 @@
 package com.adalytics.adalytics_backend.utils;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -21,16 +23,44 @@ public class NotificationHandler extends TextWebSocketHandler {
 
     private final ObjectMapper objectMapper = new ObjectMapper();
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
+    private String getUserIdFromSession(WebSocketSession session) {
+        String query = session.getUri().getQuery();
+        if (query != null) {
+            for (String param : query.split("&")) {
+                String[] keyValue = param.split("=");
+                if (keyValue.length == 2 && "token".equals(keyValue[0])) {
+                    DecodedJWT validatedToken = jwtUtil.validateToken(keyValue[1]);
+                    System.out.println("Validated token: " + validatedToken.getSubject());
+                    return validatedToken.getSubject();
+                }
+            }
+        }
+        return null;
+    }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        // Store the session
-        sessions.put(ContextUtil.getCurrentUserId(), session);
+        String userId = getUserIdFromSession(session);
+        if (userId != null) {
+            sessions.put(userId, session);
+        } else {
+            log.warn("User ID not found in WebSocket session");
+            session.close(CloseStatus.BAD_DATA);
+        }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         // Remove the session
-        sessions.remove(ContextUtil.getCurrentUserId());
+        String userId = getUserIdFromSession(session);
+        if (userId != null) {
+            sessions.remove(userId);
+        } else {
+            log.warn("User ID not found in WebSocket session");
+        }
     }
 
     @Override
@@ -44,6 +74,7 @@ public class NotificationHandler extends TextWebSocketHandler {
         // Assume the payload contains the type of notification and the message
         try {
             WebSocketSession webSocketSession = sessions.get(ContextUtil.getCurrentUserId());
+            System.out.println(webSocketSession);
             if (webSocketSession != null && webSocketSession.isOpen()) {
                 Map<String, String> notification = Map.of(
                         "type", eventType,
@@ -51,6 +82,7 @@ public class NotificationHandler extends TextWebSocketHandler {
                 );
                 String jsonNotification = objectMapper.writeValueAsString(notification);
                 webSocketSession.sendMessage(new TextMessage(jsonNotification));
+                System.out.println("Notification sent");
             }
         } catch (Exception ex) {
             log.error("Error sending pusher notification : ", ex);
